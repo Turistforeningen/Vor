a741c00f3a73a7a31d22bf6dad0cf0f23ba4f876
Ali Kaafarani <ali@kvikshaug.no>
2015-03-12 19:36:15 +0100

    Proof of concept ☃

--- static/js/pulse.js
+++ static/js/pulse.js
@@ -1,10 +1,49 @@
 $(function() {
 
-  var context = {
-    count: 2389
-  };
+  var widgets = [];
+  var currentWidget = 0;
 
-  var template = Handlebars.compile($("#activity-widget-template").html());
-  var html = template(context);
-  $(".view").empty().append(html);
+  function updateWidgets() {
+    $.ajax({
+      url: '/widgets.json',
+    }).success(function(result) {
+      widgets = result;
+      iterate();
+    }).always(function(result) {
+      setTimeout(updateWidgets, 2000);
+    });
+  }
+  updateWidgets();
+
+  function iterate() {
+    console.log("Currentwidget:", currentWidget)
+
+    var widget_name = widgets[currentWidget];
+    console.log("Name:", widget_name)
+
+    $.ajax({
+      url: '/widgets-' + widget_name + '.html',
+    }).success(function(result) {
+      var template = Handlebars.compile(result);
+
+      $.ajax({
+        url: '/widgets-' + widget_name + '.json',
+      }).success(function(result) {
+        context = result;
+        var html = template(context);
+        $(".view").empty().append(html);
+      }).always(function(result) {
+        // setTimeout(iterate, 2000);
+      });
+
+    });
+
+    // setTimeout(iterate, 2000);
+    currentWidget += 1;
+    if(currentWidget >= widgets.length) {
+      currentWidget = 0;
+    }
+
+  }
+
 });
